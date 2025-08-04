4fc436d8c2e7f2f276ee9c896e83f63dede8cb76
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-04 17:07:18 -0300

    LP - feat: adiciona connector do banco de Dados

--- Quizzer/DAO/ConectorDatabase.java
+++ Quizzer/DAO/ConectorDatabase.java
@@ -1,4 +1,23 @@
 package Quizzer.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectorDatabase {
+
+    //alterar os valores e criar banco no RAILWAY
+    private static final String URL = "jdbc:mysql://shinkansen.proxy.rlwy.net:14508/railway";
+    private static final String USER = "root";
+    private static final String SENHA = "zWzbiDPJNfHqcKbGEdROrutRQQBEGLZa";
+
+    public static Connection getConexao() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, SENHA);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
+        }
+    }
 }
